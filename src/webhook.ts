f7fda8fee616c235cc24c194e9907da4961695d9
sunzongzheng <sunzongzhengwy@163.com>
2018-05-25 12:59:22 +0800

    update webhook [ci skip]

--- src/webhook.ts
+++ src/webhook.ts
@@ -1,6 +1,7 @@
 import http from 'http'
 import createHandler from 'github-webhook-handler'
 import config from 'config'
+import { exec } from 'child_process'
 
 const webhookConfig: Config.webhook = config.get('webhook')
 
@@ -23,6 +24,13 @@ handler.on('error', err => {
 })
 
 handler.on('push', event => {
+  if (event.payload.ref === 'refs/heads/master') {
+    exec(['cd ../', 'git pull', 'npm i'].join(' && '), err => {
+      if (err instanceof Error) {
+        throw err
+      }
+    })
+  }
   console.log(
     'Received a push event for %s to %s',
     event.payload.repository.name,
