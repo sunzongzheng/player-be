3ce172228669221865e50e66e7131aaadfc946d0
sunzongzheng <sunzongzhengwy@163.com>
2018-09-06 22:09:01 +0800

    Remove data migrate job in cron

--- src/cron.ts
+++ src/cron.ts
@@ -1,5 +1,5 @@
 import { CronJob } from 'cron'
-import { updateSongInfo, move, getNeteaseRank } from './scripts'
+import { updateSongInfo, getNeteaseRank } from './scripts'
 
 export default function() {
     const job = new CronJob(
@@ -8,8 +8,6 @@ export default function() {
             console.info('定时任务开始')
             // 更新歌曲信息
             await updateSongInfo()
-            // 旧数据迁移
-            await move()
             // 获取网易云排行榜
             await getNeteaseRank()
             console.info('定时任务结束')
